ae81278199737a3aefad050d2da10c071760bc54
dimatymchak-oss <dimatymchak@gmail.com>
2026-06-01 22:25:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -897,14 +897,20 @@ return;
         
         if (type === "SmartContractExec" || action.SmartContractExec) {
   const exec = action.SmartContractExec || {};
+
   const operation = String(exec.operation || "").toLowerCase();
 
-  if (operation.includes("dedustswap") && !operation.includes("payout")) {
+  if (
+    operation.includes("dedustswap") &&
+    !operation.includes("payout")
+  ) {
     const attached = Number(exec.ton_attached || 0);
     const attachedTon = attached / 1e9;
 
-    if (attachedTon > tonAmount) {
-      tonAmount = attachedTon;
+    const realTon = attachedTon - 0.25;
+
+    if (realTon > tonAmount && realTon > 0) {
+      tonAmount = realTon;
     }
   }
 }
